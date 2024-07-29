b9ad66e90bf7db3f29e7789c578df69719281f4e
Jacob <jacobjwmorton@hotmail.com>
2024-07-29 14:23:59 +0100

    navbar

--- blog-stat/src/App.js
+++ blog-stat/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Navbar/>
     </div>
   );
 }
-
-export default App;
